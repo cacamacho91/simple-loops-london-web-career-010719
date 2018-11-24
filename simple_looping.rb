e2cafcf571f1daaed1a33205a37daa56836b3923
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-24 22:43:04 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -23,7 +23,7 @@ end
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 1
-  while  number_of_times >  counter
+  while  counter != number_of_times
     puts phrase
     counter += 1
   end
